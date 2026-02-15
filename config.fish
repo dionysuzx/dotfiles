27bdb5b43a5d09ee19c548a9144ea1c14b100dc9
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2026-02-14 21:59:31 -0500

    Add ~/.bun/bin to fish PATH

--- config.fish
+++ config.fish
@@ -43,7 +43,7 @@ end
 
 set -gx EDITOR nvim
 set -gx VISUAL nvim
-set -gx PATH $HOME/.local/bin $PATH
+set -gx PATH $HOME/.bun/bin $HOME/.local/bin $PATH
 set -gx LIMA_WORKDIR /home/lucy.linux
 set -gx CLAUDE_CODE_HIDE_ACCOUNT_INFO 1
 set -gx IS_DEMO true
